e48f7a43fafee0a9689a4f628656cf2f95396fe6
Lakshya8600 <lakshya.kumrawat@gmail.com>
2022-01-05 10:38:28 +0530

    50% inventory Done

--- main.py
+++ main.py
@@ -3,27 +3,33 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///databases/Inventory.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///Inventory.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 Inventorydb = SQLAlchemy(app)
 
 # I created Seperate database for inventory It Would be needed
-class User(Inventorydb.Model):
-    srno = Inventorydb.Column(Inventorydb.Integer, primary_key=True)
+class InventoryClass(Inventorydb.Model):
+    sno = Inventorydb.Column(Inventorydb.Integer, primary_key=True)
     ItemName = Inventorydb.Column(Inventorydb.String(30),  nullable=False)
     Quantity = Inventorydb.Column(Inventorydb.Integer, nullable=False)
     PerCost = Inventorydb.Column(Inventorydb.Integer, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.srno
+        return f"{self.sno} "
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/Inventory')
+@app.route('/Inventory', methods=['GET','POST'])
 def Inventory():
-    return render_template('Inventory.html')
+    if request.method == 'POST':
+        InventoryData = InventoryClass(ItemName=request.form['ItemName'],Quantity=request.form['ItemQuantity'],PerCost=request.form['ItemPerCost'])
+        Inventorydb.session.add(InventoryData)
+        Inventorydb.session.commit()
+
+    InventoryList = InventoryClass.query.all()
+    return render_template('Inventory.html' , InventoryList=InventoryList )
 
 
 if __name__ == '__main__':
